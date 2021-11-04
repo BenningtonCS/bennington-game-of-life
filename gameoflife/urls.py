d4490ccc1dad381a1363da56aa5c5e5a13882a32
Quang Tran <tquang1708@gmail.com>
2021-11-03 21:43:13 -0400

    adding files

--- gameoflife/urls.py
+++ gameoflife/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from simulation import views
+
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
 ]
+
+print(urlpatterns)
