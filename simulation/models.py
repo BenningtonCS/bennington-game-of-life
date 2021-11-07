abd3411a5ca9c1ca379606a7361fc52c61972afc
Dena Omar <87335652+denaomar@users.noreply.github.com>
2021-11-07 15:36:24 -0500

    created the model

--- simulation/models.py
+++ simulation/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
+class Pattern(models.Model):
+    name = models.CharField(max_length=150)
+    pattern = JSONField(max_length=500)
+    
+
--- simulation/views.py
+++ simulation/views.py
@@ -1,6 +1,10 @@
+from django.db import models
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Pattern 
 # Create your views here.
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = "simulation/index.html"
+    model = Pattern
+    
